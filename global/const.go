9b5a543d9f7c0710f3f1c5a7f9af3646e75cf1bf
lzy3240 <35475326+lzy3240@users.noreply.github.com>
2026-08-24 10:48:39 +0800

    Update const.go

--- global/const.go
+++ global/const.go
@@ -9,7 +9,7 @@ const (
 	LogFileDir       = "logs"
 	DBFileName       = "easy_pwd"
 	Version          = "v1.0.0"
-	ProjectURL       = "https://www.gitee.com"
+	ProjectURL       = "https://github.com/lzy3240/easy_pwd"
 	Author           = "zhenyi.law"
 	Contact          = "lzy3240@qq.com"
 	QuestionString   = "你最喜欢的颜色是什么？\n你第一只宠物的名字是什么？\n你家乡的名字是什么？\n你最喜欢的一部电影是什么？\n你最喜欢的一首歌是什么？\n你最喜欢的一位老师的名字是什么？\n你第一所学校的名称是什么？"
